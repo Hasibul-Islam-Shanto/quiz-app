f59a9dc6a84c062b58f496a9c2386c4b9fa1816f
Hasibul-Islam-Shanto <mdhasibulislamshanto007@gmail.com>
2025-01-15 15:42:39 +0600

    fix: changes logo and other text updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="mt-6 mb-3 opacity-40 text-center">
-      Copyright &copy; 2024 Learn With Sumit | All Rights Reserved
+      Copyright &copy; 2025 Hasibul Islam Shanto | All Rights Reserved
     </footer>
   );
 };
